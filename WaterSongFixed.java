cda42ce7a783a52a3e579b132e9e0be3bff01ef6
ujala98 <48207862+ujala98@users.noreply.github.com>
2019-03-05 01:28:09 -0800

    Git commits

---
+++ WaterSongFixed.java
@@ -0,0 +1,27 @@
+public class WaterSongFixed{
+    public static void main (String[] args){
+      int waterNum=9;
+      String word="bottles";
+     while(waterNum>0)
+  {
+    
+     System.out.println( waterNum+" "+word+"of water on the wall");
+     System.out.println(waterNum+" "+word+"of water");
+     System.out.println(" Take one down");
+     System.out.println(" Pass it around");
+      waterNum= waterNum-1;
+    if(waterNum==1)
+  {
+     word="bottle";
+  }
+   if(waterNum>0)
+ {
+     System.out.println( waterNum+" "+word+"of water on tha wall");
+ }
+    else
+ {
+    System.out.println(" No more bottles of water on the wall");
+   }
+  }
+ }
+}
